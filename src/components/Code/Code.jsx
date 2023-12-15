9cec2206fb99158f98ee9db362e4118a920e6355
yersonargote <yersonargotev@unicauca.edu.co>
2023-12-14 22:16:03 -0500

    Update Code.css, Code.jsx, Table.astro, and Layout.astro

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -1,39 +1,34 @@
-import { h } from "preact";
-import { useState } from "preact/hooks";
 import "@/components/Code/Code.css";
+import { useState } from "preact/hooks";
 
-export function CodeBlock ({ language, code }) {
-  const [buttonText, setButtonText] = useState("Copy");
+export function CodeBlock({ language, code }) {
+	const [buttonText, setButtonText] = useState("Copy");
 
-  const copyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(code);
-      setButtonText("Copied!");
-      setTimeout(() => setButtonText("Copy"), 2000);
-    } catch {
-      setButtonText("Failed to copy!");
-      setTimeout(() => setButtonText("Copy"), 2000);
-    }
-  };
+	const copyToClipboard = async () => {
+		try {
+			await navigator.clipboard.writeText(code);
+			setButtonText("Copied!");
+			setTimeout(() => setButtonText("Copy"), 2000);
+		} catch {
+			setButtonText("Failed to copy!");
+			setTimeout(() => setButtonText("Copy"), 2000);
+		}
+	};
 
-  return (
-    <section class="code-container">
-      <header class="code-header">
-        <span class="code-language">{language}</span>
-        <div>
-          <button
-            id="copy-button"
-            class="copy-button"
-            onClick={copyToClipboard}
-          >
-            {buttonText}
-          </button>
-        </div>
-      </header>
-      <hr class="code-hr" />
-      <pre id="code-block" class="code-block">
-        <code>{code}</code>
-      </pre>
-    </section>
-  );
-};
+	return (
+		<section class="code-container">
+			<header class="code-header">
+				<span class="code-language">{language}</span>
+				<div>
+					<button type="button" class="copy-button" onClick={copyToClipboard}>
+						{buttonText}
+					</button>
+				</div>
+			</header>
+			<hr class="code-hr" />
+			<pre id="code-block" class="code-block">
+				<code>{code}</code>
+			</pre>
+		</section>
+	);
+}
